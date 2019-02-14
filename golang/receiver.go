eea8c953e113a46657ef15b91170c3c5d53b110c
Kranthi Kumar Bitra <kranthi.b76@gmail.com>
2019-02-14 08:27:44 -0600

    Create receiver.go

---
+++ golang/receiver.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"posx_mq"
+)
+
+const maxTickNum = 10
+
+func main() {
+	oflag := posix_mq.O_RDONLY
+	mq, err := posix_mq.NewMessageQueue("/posix_mq_example", oflag, 0666, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer mq.Close()
+
+	fmt.Println("Start receiving messages")
+
+	count := 0
+	for {
+		count++
+
+		msg, _, err := mq.Receive()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf(string(msg))
+
+		if count >= maxTickNum {
+			break
+		}
+	}
+}
