e210b5f94f39e2596f0af0014c997b59cd7e08e5
Kranthi Kumar Bitra <kranthi.b76@gmail.com>
2019-02-14 01:53:57 -0600

    Update PosixMQ.java

--- java/PosixMQ.java
+++ java/PosixMQ.java
@@ -3,7 +3,7 @@ public class PosixMQ {
  public native int mq_close(long desc);
  public static void main(String[] args) {
   PosixMQ pmq = new PosixMQ();
-  long desc = pmq.mq_open("/JNIMQ_1",64);
+  long desc = pmq.mq_open("/test_queue",64);
   if(desc!=-1L){
    System.out.println("We have MQ open.");
    System.out.println("Close returned " + pmq.mq_close(desc));
