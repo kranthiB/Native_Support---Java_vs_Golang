6548e534ced44783ff7195207b369cd329a98381
Kranthi Kumar Bitra <kranthi.b76@gmail.com>
2019-02-14 01:53:36 -0600

    Create PosixMQ.java

---
+++ java/PosixMQ.java
@@ -0,0 +1,17 @@
+public class PosixMQ {
+ public native long mq_open(String name, int flags);
+ public native int mq_close(long desc);
+ public static void main(String[] args) {
+  PosixMQ pmq = new PosixMQ();
+  long desc = pmq.mq_open("/JNIMQ_1",64);
+  if(desc!=-1L){
+   System.out.println("We have MQ open.");
+   System.out.println("Close returned " + pmq.mq_close(desc));
+  }else{
+   System.out.println("Open failed.");
+  }
+ }
+ static {
+  System.loadLibrary("PosixMQ");
+ }
+}
